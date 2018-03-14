c09f274c6df054f7916475439b7bc82870f7249a
RaleighXc <974391165@qq.com>
2018-03-14 21:07:53 +0800

    first commir

---
+++ 2017-12-20 214426/js/2.js
@@ -0,0 +1,8 @@
+// import { A, test, Hello } from './1.js';
+// console.log(A, test, Hello);
+
+// import * as lesson from './1.js';
+// console.log(lesson);
+
+import lesson17 from './1.js';
+console.log(lesson17);
